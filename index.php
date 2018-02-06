f762eaadbea4a97b5ebf885004899214c05c666d
Philippe <contacts@madi-consultants.fr>
2018-02-06 11:30:00 +0100

    Modification titre

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $result = $link->query('SELECT id, title FROM post');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog v2</title>
+    <title>Blog v2 </title>
 </head>
 <body>
 
