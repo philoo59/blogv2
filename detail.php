929ea93e062c5bec0fab6d32d6ec9b662a9375d7
Philippe <contacts@madi-consultants.fr>
2018-02-06 16:07:36 +0100

    Correction die

--- detail.php
+++ detail.php
@@ -6,16 +6,13 @@ if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
     $row = getPost($id);
-
     if (!$row) {
         header ("HTTP/1.1 404 Not Found");
-        die;
+        die();
     }
-
-
     include ('templates/showDetail.php');
 
 } else {
     header ("HTTP/1.1 404 Not Found");
-    die;
+    die();
 }
